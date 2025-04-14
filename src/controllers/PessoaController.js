29bf83c18a4c4a64a552fb63d7d85af37e5260be
Jamil Campeão <jamilcampeao@gmail.com>
2025-04-14 00:13:08 -0300

    Criado rotas e controllers de pessoas

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,13 @@
+class PessoaController {
+  static async pegaTodas(req, res) {
+    try {
+      // Acessa o Modelo
+
+    } catch (error) {
+      //Trato o erro
+      
+    }
+  }
+}
+
+module.exports = PessoaController;
